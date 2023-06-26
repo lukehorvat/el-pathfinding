bc8ce91d987a84acdc27974c9f151378d18472ce
Luke Horvat <lukehorvat@gmail.com>
2023-06-26 13:29:49 +0200

    Display start and end coordinates

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Map } from './Map';
 import maps from '../lib/maps';
 import './App.css';
@@ -6,29 +6,78 @@ import './App.css';
 export const App: React.FC = () => {
   const [selectedMap, setSelectedMap] = useState<string>(maps[0].name);
   const [showMapImage, setShowMapImage] = useState<boolean>(true);
+  const [startPosition, setStartPosition] = useState<{
+    x: number;
+    y: number;
+  } | null>(null);
+  const [endPosition, setEndPosition] = useState<{
+    x: number;
+    y: number;
+  } | null>(null);
+
+  useEffect(() => {
+    setStartPosition(null);
+    setEndPosition(null);
+  }, [selectedMap]);
 
   return (
     <>
-      <div>
-        <label>Map:</label>
-        <select
-          value={selectedMap}
-          onChange={(event) => setSelectedMap(event.target.value)}
-        >
-          {maps.map(({ name, title }) => (
-            <option key={name} value={name}>
-              {title}
-            </option>
-          ))}
-        </select>
-        <input
-          type="checkbox"
-          checked={showMapImage}
-          onChange={(event) => setShowMapImage(event.target.checked)}
+      <form>
+        <div className="form-map-selector">
+          <label>Map:</label>
+          <select
+            value={selectedMap}
+            onChange={(event) => setSelectedMap(event.target.value)}
+          >
+            {maps.map(({ name, title }) => (
+              <option key={name} value={name}>
+                {title}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-map-image-toggle">
+          <label>Show map image:</label>
+          <input
+            type="checkbox"
+            checked={showMapImage}
+            onChange={(event) => setShowMapImage(event.target.checked)}
+          />
+        </div>
+        <div className="form-start-position">
+          <label>Start position:</label>
+          <input
+            type="text"
+            value={
+              startPosition
+                ? `${startPosition.x},${startPosition.y}`
+                : 'Not set'
+            }
+            disabled
+          />
+        </div>
+        <div className="form-end-position">
+          <label>End position:</label>
+          <input
+            type="text"
+            value={
+              endPosition ? `${endPosition.x},${endPosition.y}` : 'Not set'
+            }
+            disabled
+          />
+        </div>
+      </form>
+      {selectedMap && (
+        <Map
+          mapName={selectedMap}
+          showMapImage={showMapImage}
+          startPosition={startPosition}
+          endPosition={endPosition}
+          onPositionClick={(position, hasShiftKey) =>
+            hasShiftKey ? setEndPosition(position) : setStartPosition(position)
+          }
         />
-        <label>Show map image?</label>
-      </div>
-      {selectedMap && <Map mapName={selectedMap} showMapImage={showMapImage} />}
+      )}
     </>
   );
 };
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback } from 'react';
 import { Stage, Graphics, Sprite } from '@pixi/react';
 import { Graphics as PixiGraphics } from '@pixi/graphics';
+import { ColorSource as PixiColorSource } from '@pixi/color';
 import { MapData, useMap } from '../hooks/useMap';
 import { findPath, Graph } from '../lib/pathfinder';
 import './Map.css';
@@ -8,25 +9,24 @@ import './Map.css';
 const canvasWidth = 600;
 const canvasHeight = 600;
 
-export const Map: React.FC<{ mapName: string; showMapImage: boolean }> = ({
+export const Map: React.FC<{
+  mapName: string;
+  showMapImage: boolean;
+  startPosition: { x: number; y: number } | null;
+  endPosition: { x: number; y: number } | null;
+  onPositionClick: (
+    position: { x: number; y: number },
+    hasShiftKey: boolean
+  ) => void;
+}> = ({
   mapName,
   showMapImage,
+  startPosition,
+  endPosition,
+  onPositionClick,
 }) => {
   const mapRequest = useMap(mapName);
   const map = mapRequest.data;
-  const [start, setStart] = useState<{
-    x: number;
-    y: number;
-  } | null>();
-  const [end, setEnd] = useState<{
-    x: number;
-    y: number;
-  } | null>();
-
-  useEffect(() => {
-    setStart(null);
-    setEnd(null);
-  }, [map]);
 
   const onClick = useCallback(
     (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
@@ -45,14 +45,10 @@ export const Map: React.FC<{ mapName: string; showMapImage: boolean }> = ({
         tileY < map!.height &&
         !!map!.tiles[tileX][tileY] // Is walkable?
       ) {
-        if (event.shiftKey) {
-          setEnd({ x: tileX, y: tileY });
-        } else {
-          setStart({ x: tileX, y: tileY });
-        }
+        onPositionClick({ x: tileX, y: tileY }, event.shiftKey);
       }
     },
-    [map]
+    [map, onPositionClick]
   );
 
   if (mapRequest.isLoading) return <div>Loading map...</div>;
@@ -68,9 +64,15 @@ export const Map: React.FC<{ mapName: string; showMapImage: boolean }> = ({
     >
       {showMapImage && <MapImage mapName={mapName} />}
       {!showMapImage && <UnwalkableTiles map={map!} />}
-      {start && end && <PathTiles map={map!} start={start} end={end} />}
-      {start && <StartMarker map={map!} start={start} />}
-      {end && <EndMarker map={map!} end={end} />}
+      {startPosition && endPosition && (
+        <PathTiles map={map!} start={startPosition} end={endPosition} />
+      )}
+      {startPosition && (
+        <MapMarker map={map!} position={startPosition} color="#0000e9" />
+      )}
+      {endPosition && (
+        <MapMarker map={map!} position={endPosition} color="#e90000" />
+      )}
     </Stage>
   );
 };
@@ -117,69 +119,36 @@ export const UnwalkableTiles: React.FC<{
   return <Graphics draw={draw} />;
 };
 
-export const StartMarker: React.FC<{
+export const MapMarker: React.FC<{
   map: MapData;
-  start: { x: number; y: number };
-}> = ({ map, start }) => {
+  position: { x: number; y: number };
+  color: PixiColorSource;
+}> = ({ map, position, color }) => {
   const tileWidth = canvasWidth / map.width;
   const tileHeight = canvasHeight / map.height;
   const markerSize = 7;
   const draw = useCallback(
     (g: PixiGraphics) => {
       g.clear();
-      g.lineStyle(4, '#0000ff');
+      g.lineStyle(4, color);
       g.moveTo(
-        start.x * tileWidth + tileWidth / 2 - markerSize,
-        (map.height - start.y - 1) * tileHeight + tileHeight / 2 - markerSize
+        position.x * tileWidth + tileWidth / 2 - markerSize,
+        (map.height - position.y - 1) * tileHeight + tileHeight / 2 - markerSize
       );
       g.lineTo(
-        start.x * tileWidth + tileWidth / 2 + markerSize,
-        (map.height - start.y - 1) * tileHeight + tileHeight / 2 + markerSize
+        position.x * tileWidth + tileWidth / 2 + markerSize,
+        (map.height - position.y - 1) * tileHeight + tileHeight / 2 + markerSize
       );
       g.moveTo(
-        start.x * tileWidth + tileWidth / 2 + markerSize,
-        (map.height - start.y - 1) * tileHeight + tileHeight / 2 - markerSize
+        position.x * tileWidth + tileWidth / 2 + markerSize,
+        (map.height - position.y - 1) * tileHeight + tileHeight / 2 - markerSize
       );
       g.lineTo(
-        start.x * tileWidth + tileWidth / 2 - markerSize,
-        (map.height - start.y - 1) * tileHeight + tileHeight / 2 + markerSize
+        position.x * tileWidth + tileWidth / 2 - markerSize,
+        (map.height - position.y - 1) * tileHeight + tileHeight / 2 + markerSize
       );
     },
-    [map, start]
-  );
-
-  return <Graphics draw={draw} />;
-};
-
-export const EndMarker: React.FC<{
-  map: MapData;
-  end: { x: number; y: number };
-}> = ({ map, end }) => {
-  const tileWidth = canvasWidth / map.width;
-  const tileHeight = canvasHeight / map.height;
-  const markerSize = 7;
-  const draw = useCallback(
-    (g: PixiGraphics) => {
-      g.clear();
-      g.lineStyle(4, '#ff0000');
-      g.moveTo(
-        end.x * tileWidth + tileWidth / 2 - markerSize,
-        (map.height - end.y - 1) * tileHeight + tileHeight / 2 - markerSize
-      );
-      g.lineTo(
-        end.x * tileWidth + tileWidth / 2 + markerSize,
-        (map.height - end.y - 1) * tileHeight + tileHeight / 2 + markerSize
-      );
-      g.moveTo(
-        end.x * tileWidth + tileWidth / 2 + markerSize,
-        (map.height - end.y - 1) * tileHeight + tileHeight / 2 - markerSize
-      );
-      g.lineTo(
-        end.x * tileWidth + tileWidth / 2 - markerSize,
-        (map.height - end.y - 1) * tileHeight + tileHeight / 2 + markerSize
-      );
-    },
-    [map, end]
+    [map, position, color]
   );
 
   return <Graphics draw={draw} />;
@@ -203,7 +172,7 @@ export const PathTiles: React.FC<{
       );
 
       for (const node of path) {
-        g.beginFill('#00ff00', 1);
+        g.beginFill('#00e900', 1);
         g.drawRect(
           node.x * tileWidth,
           (map.height - node.y - 1) * tileHeight,
