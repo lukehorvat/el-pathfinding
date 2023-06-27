a70806e2f0fa4ed0f203b2748adc1187ca925e89
Luke Horvat <lukehorvat@gmail.com>
2023-06-27 23:10:40 +0200

    Support more maps

--- src/lib/maps.ts
+++ src/lib/maps.ts
@@ -4,12 +4,56 @@ export default [
     name: 'Isla Prima',
   },
   {
+    file: 'map2',
+    name: 'White Stone',
+  },
+  {
     file: 'map3',
     name: 'Desert Pines',
   },
   {
+    file: 'map4f',
+    name: 'Ruins of Tirnym',
+  },
+  {
+    file: 'map5nf',
+    name: 'Valley of the Dwarves',
+  },
+  {
+    file: 'map6nf',
+    name: 'Portland',
+  },
+  {
+    file: 'map7',
+    name: 'Morcraven Marsh',
+  },
+  {
     file: 'map8',
     name: 'Naralik',
+  },
+  {
+    file: 'map9f',
+    name: 'Grubani Peninsula',
+  },
+  {
+    file: 'map11',
+    name: 'Tarsengaard',
+  },
+  {
+    file: 'map12',
+    name: 'Nordcarn',
+  },
+  {
+    file: 'map13',
+    name: 'Southern Kilaran',
+  },
+  {
+    file: 'map14f',
+    name: 'Kilaran Field',
+  },
+  {
+    file: 'map15f',
+    name: 'Tahraji Desert',
   },
   {
     file: 'anitora',
@@ -20,7 +64,95 @@ export default [
     name: 'Idaloran',
   },
   {
+    file: 'cont2map2',
+    name: 'Bethel',
+  },
+  {
+    file: 'cont2map3',
+    name: 'Sedicolis',
+  },
+  {
+    file: 'cont2map4',
+    name: 'Melinis',
+  },
+  {
+    file: 'cont2map5',
+    name: 'Palon Vertas',
+  },
+  {
+    file: 'cont2map6',
+    name: 'Thelinor',
+  },
+  {
+    file: 'cont2map7',
+    name: 'Irsis',
+  },
+  {
+    file: 'cont2map8',
+    name: 'Emerald Valley Trade Route',
+  },
+  {
+    file: 'cont2map9',
+    name: 'Kusamura Jungle',
+  },
+  {
+    file: 'cont2map10',
+    name: 'Zirakinbar',
+  },
+  {
+    file: 'cont2map11',
+    name: 'Willowvine Forest',
+  },
+  {
+    file: 'cont2map12',
+    name: 'Aeth Aelfan',
+  },
+  {
+    file: 'cont2map13',
+    name: 'Egratia Point',
+  },
+  {
+    file: 'cont2map14',
+    name: 'Arius',
+  },
+  {
+    file: 'cont2map15',
+    name: 'North Redmoon',
+  },
+  {
+    file: 'cont2map16',
+    name: 'South Redmoon',
+  },
+  {
+    file: 'cont2map17',
+    name: 'Hurquin',
+  },
+  {
     file: 'cont2map18',
     name: 'Trassian',
+  },
+  {
+    file: 'cont2map19',
+    name: 'Hulda',
+  },
+  {
+    file: 'cont2map20',
+    name: 'Isle of the Forgotten',
+  },
+  {
+    file: 'cont2map21',
+    name: 'Imbroglio Islands',
+  },
+  {
+    file: 'cont2map22',
+    name: 'Irinveron',
+  },
+  {
+    file: 'cont2map23',
+    name: 'Glacmor',
+  },
+  {
+    file: 'cont2map24',
+    name: 'Iscalrith',
   },
 ];
