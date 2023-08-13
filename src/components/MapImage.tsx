620a243d1a6644605452bf10854ab883ba29e732
Luke Horvat <lukehorvat@gmail.com>
2023-08-14 01:16:56 +0200

    ESM fix allowing `PIXI.Texture.fromBuffer` to be used

--- src/components/MapImage.tsx
+++ src/components/MapImage.tsx
@@ -1,39 +1,31 @@
 import React, { useMemo } from 'react';
 import { useAtomValue } from 'jotai';
 import { Sprite } from '@pixi/react';
+import * as PIXI from 'pixi.js';
 import atoms from '../lib/atoms';
 
 export const MapImage: React.FC = () => {
   const mapImageInfo = useAtomValue(atoms.mapImageInfo);
   const canvasWidth = useAtomValue(atoms.canvasWidth);
   const canvasHeight = useAtomValue(atoms.canvasHeight);
-  const offscreenImageCanvas = useMemo(() => {
+  const texture = useMemo(() => {
     if (mapImageInfo.state !== 'hasData') {
       return;
     }
 
-    // Render the map's pixel data onto an offscreen canvas.
     const { rgbaData, imageWidth, imageHeight } = mapImageInfo.data;
-    const canvas = document.createElement('canvas');
-    canvas.width = imageWidth;
-    canvas.height = imageHeight;
-    const ctx = canvas.getContext('2d')!;
-    const imageData = ctx.createImageData(imageWidth, imageHeight);
-    for (let i = 0; i < imageData.data.length; i++) {
-      imageData.data[i] = rgbaData[i];
-    }
-    ctx.putImageData(imageData, 0, 0);
-
-    return canvas;
+    return PIXI.Texture.fromBuffer(rgbaData, imageWidth, imageHeight, {
+      scaleMode: PIXI.SCALE_MODES.LINEAR,
+    });
   }, [mapImageInfo]);
 
-  if (!offscreenImageCanvas) {
+  if (!texture) {
     return null;
   }
 
   return (
     <Sprite
-      source={offscreenImageCanvas}
+      texture={texture}
       x={0}
       y={0}
       width={canvasWidth}
